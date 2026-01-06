8478d99f6a465047b32f87a3978742d6b1f5a091
liuyansong98 <67949950+liuyansong98@users.noreply.github.com>
2026-01-06 14:08:37 +0800

    Change GPU settings and add parameters for training
    Updated GPU assignment and added parameters for online training.

--- DiMNet-main/src/run_ice14.sh
+++ DiMNet-main/src/run_ice14.sh
@@ -7,9 +7,9 @@ python main.py -d ICEWS14 --history_len 10 --num_head 4 --num_ly 3 --topk 50 --d
 python main.py -d ICEWS14 --history_len 10 --num_head 4 --num_ly 3 --topk 50 --decay 1e-4 --gpu 1 --test 1
 
 # online train valid
-python main.py -d ICEWS14 --history_len 10 --num_head 4 --num_ly 3 --topk 50 --decay 1e-4 --gpu 2 --test 2 \
+python main.py -d ICEWS14 --history_len 10 --num_head 4 --num_ly 3 --topk 50 --decay 1e-4 --gpu 1 --test 2 \
 --temperature 2 --distill_weight 1 --con_description continual_learning
 
 # online train test
-python main.py -d ICEWS14 --history_len 10 --num_head 4 --num_ly 3 --topk 50 --decay 1e-4 --gpu 2 --test 3 \
+python main.py -d ICEWS14 --history_len 10 --num_head 4 --num_ly 3 --topk 50 --decay 1e-4 --gpu 1 --test 3 \
 --temperature 2 --distill_weight 1 --con_description continual_learning
