ccd058d3be29a33ac05a48ce20fbf8c8e19e9c1f
liuyansong98 <67949950+liuyansong98@users.noreply.github.com>
2026-01-06 14:08:54 +0800

    Update run_ice18.sh

--- DiMNet-main/src/run_ice18.sh
+++ DiMNet-main/src/run_ice18.sh
@@ -4,12 +4,12 @@
 python main.py -d ICEWS18 --history_len 4 --num_head 4 --num_ly 3 --topk 30 --decay 1e-4 --gpu 2 --test 0
 
 # test
-python main.py -d ICEWS18 --history_len 4 --num_head 4 --num_ly 3 --topk 30 --decay 1e-4 --gpu 7 --test 1
+python main.py -d ICEWS18 --history_len 4 --num_head 4 --num_ly 3 --topk 30 --decay 1e-4 --gpu 2 --test 1
 
 # online train valid
-python main.py -d ICEWS18 --history_len 4 --num_head 4 --num_ly 3 --topk 30 --decay 1e-4 --gpu 7 --test 2 \
+python main.py -d ICEWS18 --history_len 4 --num_head 4 --num_ly 3 --topk 30 --decay 1e-4 --gpu 2 --test 2 \
 --temperature 2 --distill_weight 1 --con_description continual_learning
 
 # online train test
-python main.py -d ICEWS18 --history_len 4 --num_head 4 --num_ly 3 --topk 30 --decay 1e-4 --gpu 7 --test 3 \
+python main.py -d ICEWS18 --history_len 4 --num_head 4 --num_ly 3 --topk 30 --decay 1e-4 --gpu 2 --test 3 \
 --temperature 2 --distill_weight 1 --con_description continual_learning
