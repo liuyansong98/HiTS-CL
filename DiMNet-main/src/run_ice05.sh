0c96f2b5c31b39de27c7fe5dd0a147e3f2df1953
liuyansong98 <67949950+liuyansong98@users.noreply.github.com>
2026-01-06 14:07:55 +0800

    Update run_ice05.sh

--- DiMNet-main/src/run_ice05.sh
+++ DiMNet-main/src/run_ice05.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-## train
-#python main.py -d ICEWS05-15 --history_len 2 --num_head 1 --num_ly 1 --topk 50 --decay 1e-4 --gpu 1 --test 0
+# train
+python main.py -d ICEWS05-15 --history_len 2 --num_head 1 --num_ly 1 --topk 50 --decay 1e-4 --gpu 1 --test 0
 
 # test
-#python main.py -d ICEWS05-15 --history_len 2 --num_head 1 --num_ly 1 --topk 50 --decay 1e-4 --gpu 3 --test 1
+python main.py -d ICEWS05-15 --history_len 2 --num_head 1 --num_ly 1 --topk 50 --decay 1e-4 --gpu 1 --test 1
 
 # online train valid
-python main.py -d ICEWS05-15 --history_len 2 --num_head 1 --num_ly 1 --topk 50 --decay 1e-4 --gpu 3 --test 2 \
---temperature 2 --distill_weight 1 --con_description clod_10.10
+python main.py -d ICEWS05-15 --history_len 2 --num_head 1 --num_ly 1 --topk 50 --decay 1e-4 --gpu 1 --test 2 \
+--temperature 2 --distill_weight 1 --con_description continual_learning
 
 # online train test
-python main.py -d ICEWS05-15 --history_len 2 --num_head 1 --num_ly 1 --topk 50 --decay 1e-4 --gpu 3 --test 3 \
---temperature 2 --distill_weight 1 --con_description clod_10.10
+python main.py -d ICEWS05-15 --history_len 2 --num_head 1 --num_ly 1 --topk 50 --decay 1e-4 --gpu 1 --test 3 \
+--temperature 2 --distill_weight 1 --con_description continual_learning
